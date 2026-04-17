650715871a2f068eb55e8942dac8d31f1828bb4a
orlorlandojgr <orlanemila@gmail.com>
2026-04-17 16:55:40 -0300

    commit conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+      Scanner scan = new Scanner(System.in);
+      
+        System.out.println("Digite o número da conta: ");
+        int numeroConta = scan.nextInt();
+        System.out.println("Digite o número da agência: ");
+        String agencia = scan.next();
+        System.out.println("Digite o nome do cliente: ");
+        String nomeCliente = scan.next();
+        System.out.println("Digite o saldo da conta: ");
+        double saldo = scan.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");  
+        
+
+
     }
 }
